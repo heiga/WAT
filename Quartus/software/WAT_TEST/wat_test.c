8144ffb223dc0eb44d145fad91ad7722d0e7ea2f
Schneeraubtier <snowraptor@live.ca>
2017-02-23 18:41:43 -0700

    Added camera button ISR and started camera serial commands
    untested, WIP

--- Quartus/software/WAT_TEST/wat_test.c
+++ Quartus/software/WAT_TEST/wat_test.c
@@ -30,6 +30,7 @@
 
 #include <stdio.h>
 #include <inttypes.h>
+#include <io.h>
 #include "includes.h"
 
 /* Definition of Task Stacks */
@@ -44,6 +45,13 @@ OS_STK    wifi_test_stk[TASK_STACKSIZE];
 #define CAMERA_TEST_PRIORITY    2
 #define WIFI_TEST_PRIORITY		3
 
+/* Definition of camera commands */
+#define CAM_WRITE_OFFSET        2
+#define CAM_READ_OFFSET         2
+#define CAM_LENGTH      	    6
+static const CAM_START[CAM_LENGTH] = {0xAA, 0x01, 0x00, 0x07, 0x09, 0x03};
+static const CAM_ACK[CAM_LENGTH] = {0xAA, 0x0E, 0x00, 0x00, 0x00, 0x00}; //NEEDS REVISION
+
 /*
  * Drives FORWARD initially but when the INFRARED SENSOR
  * detects something and it will REVERSE the direction
@@ -54,18 +62,46 @@ void motor_test(void* pdata){
 	  }
 }
 
+//section 7 is the UART reference
+//https://www.altera.com/en_US/pdfs/literature/ug/ug_embedded_ip.pdf
+//
+//helpful functions for UART interfacing
+//IORD(base, offset)
+//IOWR(base, offset, data)
+
 /*
- * For this test it is assumed that the operator has two
- * monotone images ready. Upon a BUTTON being pressed the
+ * For this test it is assumed that the operator has minimum
+ * two monotone images ready. Upon a BUTTON being pressed the
  * camera will take a picture and output the average color
  * value to the terminal. Operator will switch the image and
  * press the button again. A different average color should
- * appear in the terminal
+ * appear in the terminal. Repeat as needed.
  */
 void camera_test(void* pdata){
-	  while (1){
-		  //do stuff
-	  }
+
+	//add sync commands
+
+	for (uint8_t q=0; q< CAM_LENGTH;q++){
+		IOWR(CAM_UART_BASE,CAM_WRITE_OFFSET,CAM_START[q]);
+	}
+
+	OSTimeDlyHMSM(0, 0, 3, 0);
+
+	for (uint8_t q=0; q< CAM_LENGTH;q++){
+		if (IORD(CAM_UART_BASE,CAM_WRITE_OFFSET) != CAM_ACK[q]){
+			printf("Camera start failure");
+			return;
+		}
+	}
+
+	while (1){
+		//wait for button push
+		//tell camera to take picture
+		//load image and process
+		//print to terminal
+		//repeat
+
+	}
 }
 
 /*
@@ -79,43 +115,56 @@ void wifi_test(void* pdata){
 	  }
 }
 
+static void cam_button_interrupt(void * context){
+	//clear interrupt
+	IOWR_ALTERA_AVALON_PIO_EDGE_CAP(BUTTON_BUTTON_BASE,0);
+	//post semaphore for camera
+}
+
 /* The main function creates two task and starts multi-tasking */
-int main(void)
-{
+int main(void){
   
-  OSTaskCreateExt(motor_test,
-                  NULL,
-                  (void *)&motor_test_stk[TASK_STACKSIZE-1],
-                  MOTOR_TEST_PRIORITY,
-                  MOTOR_TEST_PRIORITY,
-                  motor_test_stk,
-                  TASK_STACKSIZE,
-                  NULL,
-                  0);
+	OSTaskCreateExt(motor_test,
+                  	NULL,
+                  	(void *)&motor_test_stk[TASK_STACKSIZE-1],
+                  	MOTOR_TEST_PRIORITY,
+                  	MOTOR_TEST_PRIORITY,
+                  	motor_test_stk,
+                  	TASK_STACKSIZE,
+                  	NULL,
+                  	0);
 
-  OSTaskCreateExt(camera_test,
-                  NULL,
-                  (void *)&camera_test_stk[TASK_STACKSIZE-1],
-                  CAMERA_TEST_PRIORITY,
-                  CAMERA_TEST_PRIORITY,
-                  camera_test_stk,
-                  TASK_STACKSIZE,
-                  NULL,
-                  0);
+	OSTaskCreateExt(camera_test,
+                  	NULL,
+                  	(void *)&camera_test_stk[TASK_STACKSIZE-1],
+                  	CAMERA_TEST_PRIORITY,
+                  	CAMERA_TEST_PRIORITY,
+                  	camera_test_stk,
+                  	TASK_STACKSIZE,
+                  	NULL,
+                  	0);
 
-  OSTaskCreateExt(wifi_test,
-                  NULL,
-                  (void *)&wifi_test_stk[TASK_STACKSIZE-1],
-                  WIFI_TEST_PRIORITY,
-                  WIFI_TEST_PRIORITY,
-                  wifi_test_stk,
-                  TASK_STACKSIZE,
-                  NULL,
-                  0);
-              
+	OSTaskCreateExt(wifi_test,
+                    NULL,
+                    (void *)&wifi_test_stk[TASK_STACKSIZE-1],
+                    WIFI_TEST_PRIORITY,
+                    WIFI_TEST_PRIORITY,
+                    wifi_test_stk,
+                    TASK_STACKSIZE,
+                    NULL,
+                    0);
 
-  OSStart();
-  return 0;
+	//Interrupt controller for button controlling camera
+	alt_ic_isr_register(BUTTON_BUTTON_IRQ_INTERRUPT_CONTROLLER_ID,
+					    BUTTON_BUTTON_IRQ,
+					    cam_button_interrupt,
+					    NULL,
+					    NULL);
+	IOWR_ALTERA_AVALON_PIO_IRQ_MASK(BUTTON_BUTTON_BASE, 0x1);
+	IOWR_ALTERA_AVALON_PIO_EDGE_CAP(BUTTON_BUTTON_BASE, 0x0);
+
+	OSStart();
+	return 0;
 }
 
 /******************************************************************************
